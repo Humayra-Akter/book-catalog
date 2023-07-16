89c671596b39b790e1cadbf8a4783825dc6eacee
humayra akter <humayraakter25@gmail.com>
2023-07-16 23:07:22 +0600

    book details

--- src/pages/BookDetails.tsx
+++ src/pages/BookDetails.tsx
@@ -1,23 +1,12 @@
 import BookReview from '@/components/BookReview';
 import { Button } from '@/components/ui/button';
-import { IBook } from '@/types/globalTypes';
-import { useEffect, useState } from 'react';
+import { useSingleBookQuery } from '@/redux/features/book/bookApi';
+import { ReactElement, JSXElementConstructor, ReactFragment, Key } from 'react';
 import { useParams } from 'react-router-dom';
 
 export default function BookDetails() {
   const { id } = useParams();
-
-  //! Temporary code, should be replaced with redux
-  const [data, setData] = useState<IBook[]>([]);
-  useEffect(() => {
-    fetch('../../public/data.json')
-      .then((res) => res.json())
-      .then((data) => setData(data));
-  }, []);
-
-  const book = data?.find((item) => item._id === Number(id));
-
-  //! Temporary code ends here
+  const { data: book, isLoading, error } = useSingleBookQuery(id);
 
   return (
     <>
@@ -31,14 +20,14 @@ export default function BookDetails() {
             <small className="text-base text-blue-800 font-bold">
               Features:
             </small>
-            {book?.features?.map((feature) => (
+            {book?.features?.map((feature: string) => (
               <li key={feature}>{feature}</li>
             ))}
           </ul>
           <Button className="w-full h-6 bg-blue-800">Add to cart</Button>
         </div>
       </div>
-      <BookReview />
+      <BookReview id={id!} />
     </>
   );
 }
